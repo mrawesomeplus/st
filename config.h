66ce5722f99dbddcb4ec0db878ae9d138069245d
root <root@funk.vnetwork.com>
2022-06-09 13:57:10 -0500

    font and themeing

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "UbuntuMono:pixelsize=17:antialias=true:autohint=true";
+static char *font = "undefined-mono:pixelsize=16";
 static int borderpx = 2;
 
 /*
